9e826d797a4636aa064432c040db1f5b946f989d
gabrielsandi <146127168+gabrielsandi@users.noreply.github.com>
2023-09-28 09:17:59 -0300

    Add files via upload

---
+++ Aula 3 JS G/index.js
@@ -0,0 +1,7 @@
+//solicite para o usuario as seguintes perguntas
+let nome = prompt("Insira seu nome:")
+let cor  = prompt("Insira sua cor favorita")
+let idade = prompt("Insira sua idade")
+let cidade = prompt("Insira o nome da sua cidade")
+
+console.log("resosta:", nome, "tem", idade, "anos, mora em", cidade, "e sua cor favorita é", cor)
---
+++ Aula 3 JS G/index2.js
@@ -0,0 +1,12 @@
+let primeiroNumero = prompt("Digite o primeiro numero")
+let segundoNumero = prompt("Digite o segundo numero")
+
+//converter string para numero
+
+let numero1 = Number(primeiroNumero)
+let numero2 = Number(segundoNumero)
+
+//some os dois numeros
+
+let soma = numero1 + numero2
+console.log("resultado:", soma)
