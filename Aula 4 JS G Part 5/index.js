d33d67f2330c51d92149900e69eb1e65dd674738
gabrielsandi <146127168+gabrielsandi@users.noreply.github.com>
2023-10-10 15:25:30 -0300

    Add files via upload

---
+++ Aula 4 JS G Part 5/index.js
@@ -0,0 +1,10 @@
+let nome = prompt("Digite seu nome")
+let idade = prompt("Digite sua idade")
+let anoNasc = prompt("Qual o ano do seu nascimento")
+let anoAtual = "2023"
+
+console.log(nome)
+console.log(anoAtual - anoNasc)
+let idadeMaior = "18"
+console.log(idade >= 18)
+console.log(2050 - anoNasc)
