e25fb9c0cad52487375c775a04ea181bf8eca918
gabrielsandi <146127168+gabrielsandi@users.noreply.github.com>
2023-10-25 15:30:18 -0300

    Add files via upload

---
+++ Aula 5 JS G Part 3/index.js
@@ -0,0 +1,6 @@
+const racasDeCao = [ "Pug", "Salsicha", "Pitbull", "Pastor Alemão", "Pincher" ]
+let numeroDaRaca = prompt("Digite um número de 0 a 4")
+
+numeroDaRaca = Number(numeroDaRaca)
+
+console.log(racasDeCao[numeroDaRaca])
