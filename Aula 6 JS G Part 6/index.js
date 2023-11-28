8d65e134a830456446cd77bdb0d71444c6f8cfd9
gabrielsandi <146127168+gabrielsandi@users.noreply.github.com>
2023-11-28 14:13:07 -0300

    Add files via upload

---
+++ Aula 6 JS G Part 6/index.js
@@ -0,0 +1,13 @@
+const filme = {
+    nome: "Era uma Vez em... Hollywood",
+    diretor: "Quentin Tarantino",
+    elenco: [ "Leonardo DiCaprio", "Brad Pitt", "Margot Robbie" ],
+    lancamento: 2019,
+    visto: true,
+}
+
+console.log(filme.nome)
+console.log(filme.diretor)
+console.log(filme.elenco)
+console.log(filme["lancamento"])
+console.log(filme["visto"])
