3edd96c846e98446a37426bd5b866d41d0123b5f
gabrielsandi <146127168+gabrielsandi@users.noreply.github.com>
2023-11-28 14:23:46 -0300

    Add files via upload

---
+++ Aula 6 JS G Part 7/index.js
@@ -0,0 +1,7 @@
+const pessoa = {
+    nome: "Gabriel",
+    idade: 11,
+    generoMusicalFavorito: "música eletrônica",
+}
+
+console.log(`O nome dele é ${pessoa.nome}, ele tem ${pessoa.idade} anos e gosta muito de ${pessoa.generoMusicalFavorito}.`)
